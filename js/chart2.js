fdc40a8338dd06d29238b61e95db312ffda1ebba
Indhumathi K <indhumathi9900@gmail.com>
2020-04-13 15:05:37 +0530

    Update chart2.js

--- js/chart2.js
+++ js/chart2.js
@@ -32,7 +32,26 @@ function drawchart2(){
         });
     
         Highcharts.chart('totalDeath-container', {
-    
+    exporting: {
+                buttons: {
+                    customButton: {
+                        text: 'Linear',
+                        onclick: function() {
+                            this.yAxis[0].update({
+                                type: 'linear'
+                            });
+                        }
+                    },
+                    customButton2: {
+                        text: 'Logarithmic',
+                        onclick: function() {
+                            this.yAxis[0].update({
+                                type: 'logarithmic'
+                            });
+                        }
+                    },
+                }
+            },
             chart: {
                 height: 500
             },
